cce5d297820e9077863e5ed15343fa4f86a52675
GenVenom <51529050+GenVenom@users.noreply.github.com>
2022-12-08 10:38:47 +0545

    Update gpttest.py

--- gpttest.py
+++ gpttest.py
@@ -2,6 +2,6 @@ from revChatGPT.revChatGPT import Chatbot
 
 def get_response(prompt):
 
-    reply = chatbot.get_chat_response("How do u make turkish coffee")
+    reply = chatbot.get_chat_response(prompt)
 
     return(reply['message'])
